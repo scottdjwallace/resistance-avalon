391e0029b10e2d336c5d773d1d498d8506cc4956
Scott Wallace <ecosse_18@hotmail.com>
2015-03-01 12:55:08 -0500

    tested dynamically generated edit text and button
    button calls correct method, but method does not do anything yet

--- TheResistanceAvalon/app/src/main/java/com/lcbs/theresistanceavalon/EnterPlayerNames.java
+++ TheResistanceAvalon/app/src/main/java/com/lcbs/theresistanceavalon/EnterPlayerNames.java
@@ -1,13 +1,18 @@
 package com.lcbs.theresistanceavalon;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 
 public class EnterPlayerNames extends ActionBarActivity {
@@ -38,17 +43,36 @@ public class EnterPlayerNames extends ActionBarActivity {
 
     // Dynamically creates EditText Elements
     private void createEditTexts() {
+        int num = 1;
         for (int i = 0; i < NAMES_NEEDED; i++) {
             EditText editText = new EditText(getBaseContext());
             containerLayout.addView(editText);
             editText.setGravity(Gravity.CENTER_HORIZONTAL);
             LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) editText.getLayoutParams();
             layoutParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
-            layoutParams.setMargins(15, 0, 0, 0);
+            layoutParams.setMargins(30, 20, 30, 0);
             editText.setLayoutParams(layoutParams);
-            editText.setHint("Player " + (i+1) + "\'s Name");
-            editText.setTag("PLAYER" + (i+1));
+            editText.setHint("Player " + num + "\'s Name");
+            editText.setHintTextColor(Color.BLACK); // change these later
+            editText.setTextColor(Color.BLACK); // change these later
+            editText.setTag("PLAYER" + num);
+            editText.setId(i+1);
+            num++;
         }
+        // create submit button here
+        final Button submitButton = new Button(this);
+        submitButton.setText("Submit");
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(15,15,15,15); // left, top, right, botton
+        submitButton.setWidth(200);
+        submitButton.setGravity(Gravity.CENTER_HORIZONTAL);
+        containerLayout.addView(submitButton, lp);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                submitButton.setText("It worked");
+                saveNamesStartGame();
+            }
+        });
     }
 
     // Saves the names entered and starts new game
