e95c36dfde87baa9bd66dac961dc1fb91a41394e
Scott Wallace <ecosse_18@hotmail.com>
2015-04-05 21:11:32 -0400

    removed title bar, added colors.xml, fixed portrait only

--- TheResistanceAvalon2/app/src/main/java/com/lcbs/theresistanceavalon/StartMenu.java
+++ TheResistanceAvalon2/app/src/main/java/com/lcbs/theresistanceavalon/StartMenu.java
@@ -1,39 +1,21 @@
 package com.lcbs.theresistanceavalon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
-public class StartMenu extends ActionBarActivity {
+public class StartMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_menu, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
